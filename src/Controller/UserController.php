4257a172489cb728e920fa5846b44571d0895209
jmogmat <jmog.joaquinmatias@gmail.com>
2025-06-01 21:10:34 +0200

    [DEV] Configuration to send email using Mailtrap on mode Sending Domain

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,8 @@ use App\Service\Mailer\MailService;
 use App\Service\User\UserService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,20 +18,32 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use DateTime;
 class UserController extends AbstractController
 {
-    #[Route('/register-login', name: 'app_register_and_login')]
-    public function register(
-        Request $request,
-        UserPasswordHasherInterface $passwordHasher,
-        UserService $userService,
-        MailService $mailService,
-        AuthenticationUtils $authenticationUtils): Response
-    {
-        // Formulario de Login
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-        $loginForm = $this->createForm(LoginFormType::class);
 
-        // Formulario de Registro
+
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly UserService $userService,
+        private readonly MailService $mailService,
+        private readonly AuthenticationUtils $authenticationUtils)
+    {
+    }
+
+    #[Route('/login/', name: 'app_login')]
+    public function login(Request $request): Response
+    {
+
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $this->authenticationUtils->getLastUsername();
+        $loginForm = $this->createForm(LoginFormType::class);
+        return $this->render('user/login.html.twig', [
+            'form' => $loginForm->createView(),
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
+        }
+
+    #[Route('/register/', name: 'app_register')]
+    public function register(Request $request): Response {
         $registrationForm = $this->createForm(RegistrationFormType::class);
         $registrationForm->handleRequest($request);
 
@@ -38,22 +52,19 @@ class UserController extends AbstractController
             $email = $data['email'];
             $password = $data['password'];
 
-            $existingUser = $userService->existsUserEmail($email); //Comprobamos si el email existe en la bd
+            $existingUser = $this->userService->existsUserEmail($email);
 
-            if ($existingUser !== null) { //Si es un User entonces...
-                if ($existingUser->getRequestedToken() !== null && $existingUser->getRequestedToken() > new DateTime()) {
+            if ($existingUser !== null) {
+                if ($existingUser->getRequestedToken() !== null && $existingUser->getRequestedToken() > new \DateTime()) {
                     $this->addFlash('error', 'Ya existe un registro con este email. Verifica tu correo para validar tu cuenta.');
                 } else {
-                    try{
+                    try {
                         $existingUser->updateToken();
-                        $existingUser->setRequestedToken(new DateTime('+24 hours'));
-                        $userService->saveUser($existingUser);
-                        if($mailService->sendTokenEmail($email, $existingUser->getToken())){
-                            $this->addFlash('success', 'Se ha reenviado un nuevo email. Verifica tu correo para validar tu cuenta.');
-                        } else {
-                            $this->addFlash('error', 'No se pudo enviar el correo. Intente nuevamente más tarde.');
-                        }
-                    }catch(Exception $e) {
+                        $existingUser->setRequestedToken(new \DateTime('+24 hours'));
+                        $this->userService->saveUser($existingUser);
+                        $this->mailService->sendTokenEmail($email, $existingUser->getToken());
+                        $this->addFlash('success', 'Se ha reenviado un nuevo email. Verifica tu correo para validar tu cuenta.');
+                    } catch (\Exception $e) {
                         $this->logger->error('Error actualizando usuario: ' . $e->getMessage());
                         $this->addFlash('error', 'Ocurrió un error durante el proceso. Inténtalo de nuevo.');
                     }
@@ -61,28 +72,46 @@ class UserController extends AbstractController
             } else {
                 try {
                     $newUser = new User($email);
-                    $newUser->setPassword($passwordHasher->hashPassword($newUser, $password));
-                    $newUser->setRequestedToken(new DateTime('+24 hours'));
-                    $userService->saveUser($newUser);
-                    if($mailService->sendTokenEmail($email, $newUser->getToken())){
-                        $this->addFlash('success', 'Registro exitoso. Verifica tu correo para validar tu cuenta.');
-                    } else {
-                        $this->addFlash('error', 'No se pudo enviar el correo. Intente nuevamente más tarde.');
-                    }
-                }catch (\Exception $e){
+                    $newUser->setPassword($this->passwordHasher->hashPassword($newUser, $password));
+                    $newUser->setRequestedToken(new \DateTime('+24 hours'));
+                    $this->userService->saveUser($newUser);
+                    $this->mailService->sendTokenEmail($email, $newUser->getToken());
+                    $this->addFlash('success', 'Registro exitoso. Verifica tu correo para validar tu cuenta.');
+                } catch (\Exception $e) {
                     $this->logger->error('Error creando usuario: ' . $e->getMessage());
                     $this->addFlash('error', 'Ocurrió un error durante el registro. Inténtalo de nuevo.');
                 }
             }
+
+            return $this->redirectToRoute('app_register');
         }
 
-        return $this->render('user/register_and_login.html.twig', [
+        return $this->render('user/register.html.twig', [
             'registrationForm' => $registrationForm->createView(),
-            'form' => $loginForm->createView(),
-            'error' => $error,
-            'last_username' => $lastUsername
         ]);
+    }
+
+
+
+    #[Route('/test-email/', name: 'test_email')]
+    public function testEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('no-reply@demomailtrap.com')
+            ->to('jmog.matias@gmail.com') // cualquier correo
+            ->subject('Prueba de correo')
+            ->text('Esto es una prueba');
+
+        try {
+            $mailer->send($email);
+            return new Response('Correo enviado correctamente');
+        } catch (\Exception $e) {
+            return new Response('Error al enviar: ' . $e->getMessage());
         }
+    }
+
+
+
 
     #[Route('/logout', name: 'app_logout')]
     public function logout(): void
--- src/Service/Mailer/MailService.php
+++ src/Service/Mailer/MailService.php
@@ -15,35 +15,39 @@ use Symfony\Component\Mime\Email;
 
 class MailService
 {
+
     public function __construct(
         private readonly MailerInterface $mailer,
-        private readonly Environment $twig,
-        UrlGeneratorInterface $urlGenerator
+        private readonly UrlGeneratorInterface $urlGenerator
     ) {
-        $this->urlGenerator = $urlGenerator;
+
     }
 
-    /**
-     * @throws TransportExceptionInterface
-     */
+
     public function sendTokenEmail(string $toEmail, string $token): void
     {
         $mailTitle = 'Verificación de cuenta';
         $url = $this->generateUrl('verify_account', ['token' => $token]);
-
-        $email = (new TemplatedEmail())
-            ->from(new Address('matias.xilon@gmail.com', 'Matias Xilon'))
-            ->to(new Address($toEmail))
-            ->html(
-                sprintf(
-                    'Gracias por registrarte. Por favor, haz clic en el siguiente enlace para verificar tu cuenta: <a href="%s">%s</a>',
-                    $url,
-                    $url
-                )
-            );
+        $email = (new Email())
+                ->from(new Address('no-reply@demomailtrap.com', 'Matias AppCoffee'))
+                ->to(new Address($toEmail))
+                ->subject($mailTitle)
+                ->html(
+                    sprintf(
+                        'Gracias por registrarte. Por favor, haz click en el siguiente enlace para verificar tu cuenta: <a href="%s">%s</a>',
+                        $url,
+                        $url
+                    )
+                );
+        try {
             $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            error_log('Error enviando mail: ' . $e->getMessage());
+            throw $e;
+        }
 
     }
+
 
     public function generateUrl(string $route, array $parameters = []): string
     {
