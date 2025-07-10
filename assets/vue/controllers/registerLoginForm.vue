5421e9256f8207ddc68cfc5871118fef67649f80
jmogmat <jmog.joaquinmatias@gmail.com>
2025-07-10 13:27:55 +0200

    Updated dependencies

--- assets/vue/controllers/registerLoginForm.vue
+++ assets/vue/controllers/registerLoginForm.vue
@@ -1,3 +1,15 @@
+<template>
+  <div>
+    <h2>Register and Login</h2>
+    <form @submit.prevent="submitForm">
+      <input type="text" v-model="username" placeholder="Usuario" />
+      <input type="email" v-model="email" placeholder="Email" />
+      <input type="password" v-model="password" placeholder="Contraseña" />
+      <button type="submit">Registrar</button>
+    </form>
+  </div>
+</template>
+
 <script setup>
 import { ref } from 'vue'
 
@@ -6,7 +18,7 @@ const email = ref('')
 const password = ref('')
 
 function submitForm() {
-  // Acá podés manejar el envío de datos, por ejemplo con fetch o axios
+  // Aqui puedes manejar el envío de datos, por ejemplo con fetch o axios
   console.log('Formulario enviado:', {
     username: username.value,
     email: email.value,
@@ -20,17 +32,6 @@ function submitForm() {
 }
 </script>
 
-<template>
-  <div>
-    <h2>Register and Login</h2>
-    <form @submit.prevent="submitForm">
-      <input type="text" v-model="username" placeholder="Usuario" />
-      <input type="email" v-model="email" placeholder="Email" />
-      <input type="password" v-model="password" placeholder="Contraseña" />
-      <button type="submit">Registrar</button>
-    </form>
-  </div>
-</template>
 
 <style scoped>
 
